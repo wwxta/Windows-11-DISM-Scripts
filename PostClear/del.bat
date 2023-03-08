c673829772a052c485308ada49830f1baecf9855
wwxta <122948724+wwxta@users.noreply.github.com>
2023-03-08 02:25:06 +0000

    Add files via upload

---
+++ PostClear/del.bat
@@ -0,0 +1,10 @@
+rd /s /q "c:\System Volume Information"
+rd /s /q "C:\PerfLogs"
+del /f /q "C:\Windows\System32\ctfmon.exe"
+del /f /q "C:\Windows\System32\mobsyns.exe"
+del /f /q "C:\Windows\System32\SppExtComObj.exe"
+
+del /s /f /q C:\windows\temp\*.* 
+rd /s /q C:\windows\temp 
+del /s /f /q %temp%\*.* 
+rd /s /q %temp% 
