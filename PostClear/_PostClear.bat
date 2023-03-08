7b8901d522e4742249bad437545050320086711d
wwxta <122948724+wwxta@users.noreply.github.com>
2023-03-08 04:23:32 +0000

    Update _PostClear.bat

--- PostClear/_PostClear.bat
+++ PostClear/_PostClear.bat
@@ -6,8 +6,6 @@ EXIT /b 0
 title Wait Explorer
 taskkill /f /im explorer.exe
 if exist %programdata%\PostClear\FirstLoad.reg (
-	title Stopping DiagTrack
-	net stop DiagTrack
 	title Applying FirstLoad.reg
 	%programdata%\PostClear\AdvancedRun.exe /EXEFilename %windir%\System32\reg.exe /CommandLine "import %programdata%\PostClear\FirstLoad.reg" /RunAs 8 /WaitProcess 1 /Run
 	title Deleting Defender tasks
@@ -32,22 +30,51 @@ title Applying PostClear.reg
 reg import %programdata%\PostClear\PostClear.reg
 TIMEOUT /T 1 /NOBREAK >nul
 if exist %programdata%\PostClear\PostClearM.bat (
-	title Install CustomStart
+	title Install CustomStart	
+	!7z.exe /q INSTALLDIR="C:\Program Files\7-Zip" INSTALLLEVEL=1 
+	"C:\Program Files\7-Zip\7z.exe" x "%programdata%\PostClear\38.7z"
+	powercfg -import "%programdata%\Postclear\power.pow" 77777777-7777-7777-7777-777777777777
+	powercfg -SETACTIVE "77777777-7777-7777-7777-777777777777"
+	powercfg -delete 381b4222-f694-41f0-9685-ff5bb260df2e
+	powercfg -delete 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c 
+	powercfg -delete a1841308-3541-4fab-bc81-f71556f20b4a
 	del /f /q %programdata%\PostClear\PostClearM.bat
-	start /w %programdata%\PostClear\CustomStart.exe /S
+	start /w %programdata%\PostClear\vcredist_x64.exe /quiet
+	start /w %programdata%\PostClear\vcredist_x86.exe /quiet
 	TIMEOUT /T 2 /NOBREAK >nul
-	del /f /q %programdata%\PostClear\CustomStart.exe
-	reg add HKEY_CURRENT_USER\Software\StartIsBack /v NavBarGlass /t REG_DWORD /d 0 /f
-	reg add HKEY_CURRENT_USER\Software\StartIsBack /v Start_NotifyNewApps /t REG_DWORD /d 0 /f
-	reg add HKEY_CURRENT_USER\Software\StartIsBack /v Start_ShowControlPanel /t REG_DWORD /d 2 /f
-	reg add HKEY_CURRENT_USER\Software\StartIsBack /v Start_ShowMyMusic /t REG_DWORD /d 0 /f
-	reg add HKEY_CURRENT_USER\Software\StartIsBack /v Start_ShowRecentDocs /t REG_DWORD /d 0 /f
-	reg add HKEY_CURRENT_USER\Software\StartIsBack /v SysTrayStyle /t REG_DWORD /d 0 /f
-) else (
-	title Start Explorer
+	start /w %programdata%\PostClear\STR.exe -install
+	sc config STR start=auto
+	net start STR
+	TIMEOUT /T 2 /NOBREAK >nul
+	reg import %programdata%\PostClear\_RegistryTweaks.reg
+	!start /w %programdata%\PostClear\nvidia.exe	
+	TIMEOUT /T 2 /NOBREAK >nul
+	title 33 
+	TIMEOUT /T 4 /NOBREAK >nul
+	call %programdata%\PostClear\38.cmd
 	start %windir%\explorer.exe
+	start /w %programdata%\PostClear\NvidiaProfileInspector.exe "%programdata%\PostClear\NvidiaBaseProfile.nip" -silent
+	TIMEOUT /T 2
+	sysdm.cpl
+	!title simplewall_steam
+	!ix
+	PAUSE
 	TIMEOUT /T 2 /NOBREAK >nul
+	lodctr /r
+	lodctr /r	
+	start /w %programdata%\PostClear\cru.exe 
+	start /w %programdata%\PostClear\msi.exe
+	%programdata%\PostClear\AdvancedRun.exe /EXEFilename %windir%\System32\cmd.exe /CommandLine "call del.bat"
+	%programdata%\PostClear\AdvancedRun.exe /EXEFilename %windir%\System32\reg.exe /CommandLine "import %programdata%\PostClear\disable.reg" /RunAs 4 /WaitProcess 1 /Run
+	call %programdata%\PostClear\destroy.txt
+	%programdata%\PostClear\AdvancedRun.exe /EXEFilename %windir%\System32\cmd.exe /CommandLine "regedit" /RunAs 4 /WaitProcess 1 /Run	
+	cleanmgr /verylowdisk
+) else (
+	title meme
+	del /f /q %programdata%\PostClear\AdvancedRun.exe
+	!%programdata%\PostClear\AdvancedRun.exe /EXEFilename "" /RunAs 4 /WaitProcess 1 /Run	
 )
+
 :Reboot
 title Rebooting...
 SHUTDOWN -r -t 3
